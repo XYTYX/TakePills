b3e37d2840c2f058f436f16dbe87294f1e1e00be
XYTYX <aidan.cheng94@gmail.com>
2015-06-20 16:00:37 +0800

    ASDF

--- app/src/main/java/com/example/aidan/takepills/MainActivity.java
+++ app/src/main/java/com/example/aidan/takepills/MainActivity.java
@@ -18,8 +18,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
