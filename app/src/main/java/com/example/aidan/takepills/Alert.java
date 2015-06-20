a8f7cf3f2e1818fd21349d71a06dd4bcfc5f2bf0
XYTYX <aidan.cheng94@gmail.com>
2015-06-21 02:53:34 +0800

    Beginning Alert system

---
+++ app/src/main/java/com/example/aidan/takepills/Alert.java
@@ -0,0 +1,20 @@
+package com.example.aidan.takepills;
+
+/**
+ * Created by Aidan on 6/21/15.
+ */
+
+import android.support.v4.app.DialogFragment;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+public class Alert extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Remember to take your FUCKIGN MEDS");
+
+    }
+}
