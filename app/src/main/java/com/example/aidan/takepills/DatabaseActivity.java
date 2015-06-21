11c850e428cd650d6e4caae288f7e6b91c608b4b
Byron <byronscyoung@gmail.com>
2015-06-21 09:11:28 +0800

    .

---
+++ app/src/main/java/com/example/aidan/takepills/DatabaseActivity.java
@@ -0,0 +1,45 @@
+package com.example.aidan.takepills;
+
+/**
+ * Created by Byron on 6/21/15.
+ */
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DatabaseActivity extends ActionBarActivity {
+
+    TextView idView;
+    EditText medBox;
+    EditText doseBox;
+    EditText statusBox;
+    EditText dateBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_add_alert);
+
+        idView = (TextView) findViewById(R.id.medID);
+        medBox = (EditText) findViewById(R.id.medName);
+        doseBox = (EditText) findViewById(R.id.medDose);
+        statusBox = (EditText) findViewById(R.id.medStatus);
+        dateBox = (EditText) findViewById(R.id.medDateEnd);
+    }
+
+    public void newMed (View view) {
+        DBHandler dbHandler = new DBHandler(this, null, null, 1);
+
+        takePillsDb name = new takePillsDb();
+
+        dbHandler.addMeds(name);
+        medBox.setText("");
+        doseBox.setText("");
+        statusBox.setText("");
+        dateBox.setText("");
+    }
+}
